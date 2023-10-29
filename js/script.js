2f734bc85645c62d207b285a90135caea4ac1ebc
tomorrowTV <146773400+tomorrowTV@users.noreply.github.com>
2023-10-29 14:54:55 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
     const videoArray = [
-        'wwwroot/videos/SW1.mp4',
-        'wwwroot/videos/SW2.mp4',
-        'wwwroot/videos/SW3.mp4',
-        'wwwroot/videos/SW4.mp4',
-        'wwwroot/videos/SW5.mp4',
-        'wwwroot/videos/SW6.mp4',
+        'SW1.mp4',
+        'SW2.mp4',
+        'SW3.mp4',
+        'SW4.mp4',
+        'SW5.mp4',
+        'SW6.mp4',
         // Add more video filenames as needed
     ];
 
     const videoPlayerContainer = document.getElementById('videoPlayerContainer');
     const videoElement = document.createElement('video');
     videoElement.id = 'videoPlayer';
-    videoElement.controls = true;
-    videoElement.setAttribute('playsinline', '');
-    videoElement.preload = 'auto';
+    videoElement.controls = true; // Add controls for user interaction
+    videoElement.setAttribute('playsinline', ''); // Add playsinline attribute
+    videoElement.preload = 'auto'; // Set preload attribute to 'auto' for video
     videoPlayerContainer.appendChild(videoElement);
 
     const audioPlayer = document.createElement('audio');
-    audioPlayer.src = 'wwwroot/assets/Song.m4a';
+    audioPlayer.src = 'Song.m4a'; // Assuming the audio file is in the same directory
     audioPlayer.preload = 'auto';
     audioPlayer.load();
     document.body.appendChild(audioPlayer);
@@ -65,6 +65,14 @@ document.addEventListener('DOMContentLoaded', function () {
             console.error('Audio playback error:', error.message);
         });
     }
+
+    // Message event handler for the preloadWorker
+    preloadWorker.onmessage = (event) => {
+        // Handle the preloaded video element
+        const preloadedVideoElement = event.data;
+
+        // You can use this preloadedVideoElement in your application as needed
+    };
 
     document.addEventListener('click', () => {
         if (!audioStarted) {
