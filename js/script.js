8ec97ff3f2762012f1064f99fa518047abfa2d55
tomorrowTV <146773400+tomorrowTV@users.noreply.github.com>
2023-10-28 23:53:34 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Define an array of video filenames (without the path)
     const videoArray = [
         'wwwroot/videos/SW1.mp4',
         'wwwroot/videos/SW2.mp4',
@@ -17,88 +16,88 @@ document.addEventListener('DOMContentLoaded', function () {
     videoElement.setAttribute('playsinline', ''); // Add playsinline attribute
     videoPlayerContainer.appendChild(videoElement);
 
-    // Create an audio element and set its source
     const audioPlayer = document.createElement('audio');
     audioPlayer.src = 'wwwroot/assets/Song.m4a'; // Replace with the actual audio file path
     audioPlayer.preload = 'auto';
+    audioPlayer.load();
+    document.body.appendChild(audioPlayer);
 
-    // Create the canvas element with specified dimensions
     const canvas = document.createElement('canvas');
     canvas.width = 960;
     canvas.height = 540;
     document.body.appendChild(canvas);
 
-    // Variables to track the currently playing video index and the timer interval
     let currentVideoIndex = 0;
-    const timerInterval = 100; // 100 ms
-    let audioStarted = false; // Track whether audio has been started
-    let videosLoadedCount = 0; // Track the number of videos that have loaded
+    let audioStarted = false;
+    let videosLoadedCount = 0;
+    let startDelay = 2000; // Set the delay in milliseconds (2 seconds)
 
-    // Function to preload a video by index
     function preloadVideoByIndex(index) {
         const preloadVideo = document.createElement('video');
         preloadVideo.src = videoArray[index];
         preloadVideo.preload = 'auto';
+        preloadVideo.load();
 
-        // Use the "canplaythrough" event to track when the video is ready to play
-        preloadVideo.addEventListener('canplaythrough', () => {
+        preloadVideo.addEventListener('loadeddata', () => {
             preloadVideo.style.display = 'none';
             document.body.appendChild(preloadVideo);
-            
-            // Increment the loaded count and check if all videos are loaded
             videosLoadedCount++;
-            if (videosLoadedCount === videoArray.length) {
-                startAudio(); // Start audio when all videos are preloaded
+
+            if (videosLoadedCount === 1) {
+                setTimeout(() => {
+                    startAudio();
+                }, startDelay);
             }
         });
-
-        preloadVideo.load();
     }
 
-    // Preload all videos in the array
-    for (let i = 0; i < videoArray.length; i++) {
-        preloadVideoByIndex(i);
-    }
-
-    // Function to play video by index and synchronize with the audio
     function playVideoByIndex(index) {
         videoElement.pause();
         videoElement.src = videoArray[index];
-        videoElement.currentTime = audioPlayer.currentTime; // Synchronize with audio time
+        videoElement.currentTime = audioPlayer.currentTime;
 
-        // Add an event listener to restart video playback when it ends
         videoElement.addEventListener('ended', () => {
-            videoElement.currentTime = 0; // Reset video to the beginning
-            videoElement.play(); // Start video playback again
+            videoElement.currentTime = 0;
+            videoElement.play();
         });
 
-        // Add an event listener to start audio playback when the video is playing
         videoElement.addEventListener('play', () => {
-            if (!audioStarted) {
-                audioPlayer.load(); // Load the audio
-                audioPlayer.play()
-                    .catch(error => {
-                        // Handle any audio playback errors here
-                        console.error('Audio playback error:', error.message);
-                    });
-                audioStarted = true; // Set the flag to true to indicate audio has started
-            }
+            preloadNextVideo(index);
         });
 
         videoElement.play()
             .catch(error => {
-                // Handle any video playback errors here
                 console.error('Video playback error:', error.message);
             });
 
         currentVideoIndex = index;
     }
 
-    // Add a click event listener to start video on the first click
+    function preloadNextVideo(index) {
+        const nextIndex = (index + 1) % videoArray.length;
+        const preloadVideo = document.createElement('video');
+        preloadVideo.src = videoArray[nextIndex];
+        preloadVideo.preload = 'auto';
+
+        preloadVideo.addEventListener('canplaythrough', () => {
+            preloadVideo.style display = 'none';
+            document.body.appendChild(preloadVideo);
+        });
+
+        preloadVideo.load();
+    }
+
+    function startAudio() {
+        audioPlayer.load();
+        audioPlayer.play()
+            .catch(error => {
+                console.error('Audio playback error:', error.message);
+            });
+    }
+
     document.addEventListener('click', () => {
         if (!audioStarted) {
-            startAudio(); // Start audio on the first click
-            audioStarted = true; // Set the flag to true to indicate audio has started
+            audioStarted = true;
         }
 
         const nextIndex = (currentVideoIndex + 1) % videoArray.length;
