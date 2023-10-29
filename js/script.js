94a61b3c43ece581418141ee0e9878256a3415db
tomorrowTV <146773400+tomorrowTV@users.noreply.github.com>
2023-10-29 12:49:00 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,15 +12,14 @@ document.addEventListener('DOMContentLoaded', function () {
     const videoPlayerContainer = document.getElementById('videoPlayerContainer');
     const videoElement = document.createElement('video');
     videoElement.id = 'videoPlayer';
-    videoElement.controls = true;
-    videoElement.setAttribute('playsinline', '');
+    videoElement.controls = true; // Add controls for user interaction
+    videoElement.setAttribute('playsinline', ''); // Add playsinline attribute
     videoPlayerContainer.appendChild(videoElement);
 
     const audioPlayer = document.createElement('audio');
     audioPlayer.src = 'wwwroot/assets/Song.m4a';
     audioPlayer.preload = 'auto';
     audioPlayer.load();
-    audioPlayer.currentTime = 0; // Ensure audio starts from the beginning
     document.body.appendChild(audioPlayer);
 
     const canvas = document.createElement('canvas');
@@ -29,43 +28,61 @@ document.addEventListener('DOMContentLoaded', function () {
     document.body.appendChild(canvas);
 
     let currentVideoIndex = 0;
+    const timerInterval = 100; // 100 ms
     let audioStarted = false;
 
-    function preloadVideo(index) {
+    let preloadedVideoIndex = 1; // Start with the second video
+
+    function preloadNextVideo() {
         const preloadVideo = new Audio();
-        preloadVideo.src = videoArray[index];
+        preloadVideo.src = videoArray[preloadedVideoIndex];
         preloadVideo.preload = 'auto';
         preloadVideo.load();
+
+        preloadVideo.addEventListener('loadeddata', () => {
+            preloadVideo.style.display = 'none';
+            document.body.appendChild(preloadVideo);
+            preloadedVideoIndex = (preloadedVideoIndex + 1) % videoArray.length;
+        });
     }
 
     function playVideoByIndex(index) {
         videoElement.pause();
         videoElement.src = videoArray[index];
+        videoElement.currentTime = audioPlayer.currentTime;
 
-        videoElement.addEventListener('canplay', () => {
-            // When the video is ready, play both the video and audio
-            videoElement.play().catch(error => {
-                console.error('Video playback error:', error.message);
-            });
-
-            if (!audioStarted) {
-                audioPlayer.play().catch(error => {
-                    console.error('Audio playback error:', error.message);
-                });
-                audioStarted = true;
-            }
-
+        videoElement.addEventListener('timeupdate', () => {
+            // Update audio playback time to match video time
             audioPlayer.currentTime = videoElement.currentTime;
         });
 
         videoElement.addEventListener('ended', () => {
-            currentVideoIndex = (currentVideoIndex + 1) % videoArray.length;
-            preloadVideo(currentVideoIndex);
-            playVideoByIndex(currentVideoIndex);
+            videoElement.currentTime = 0;
+            videoElement.play();
+        });
+
+        videoElement.play().catch(error => {
+            console.error('Video playback error:', error.message);
+        });
+
+        currentVideoIndex = index;
+    }
+
+    function startAudio() {
+        audioPlayer.load();
+        audioPlayer.play().catch(error => {
+            console.error('Audio playback error:', error.message);
         });
     }
 
-    // Initial video preload
-    preloadVideo(currentVideoIndex);
-    playVideoByIndex(currentVideoIndex);
+    document.addEventListener('click', () => {
+        if (!audioStarted) {
+            startAudio();
+            audioStarted = true;
+        }
+
+        const nextIndex = (currentVideoIndex + 1) % videoArray.length;
+        playVideoByIndex(nextIndex);
+        preloadNextVideo();
+    });
 });
