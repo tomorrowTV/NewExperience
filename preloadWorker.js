0ed882a1f42d0154492dc56fadb0655ac712bdbb
tomorrowTV <146773400+tomorrowTV@users.noreply.github.com>
2023-10-29 23:19:21 -0400

    Update preloadWorker.js

--- preloadWorker.js
+++ preloadWorker.js
@@ -3,9 +3,10 @@ self.addEventListener('message', event => {
 
     // Array to hold preloaded video elements
     const preloadedVideos = [];
+    let preloadIndex = 0;
 
-    // Function to preload a single video
-    function preloadVideo(videoFileName, index) {
+    // Function to preload a single video sequentially
+    function preloadVideo(videoFileName) {
         const videoPath = 'wwwroot/videos/' + videoFileName;
         const preloadVideo = document.createElement('video');
         preloadVideo.src = videoPath;
@@ -13,18 +14,24 @@ self.addEventListener('message', event => {
 
         preloadVideo.addEventListener('loadeddata', () => {
             // Add the preloaded video element to the array
-            preloadedVideos[index] = preloadVideo;
+            preloadedVideos.push(preloadVideo);
 
             // Check if all videos are preloaded
             if (preloadedVideos.length === videoFileNames.length) {
                 // Post all preloaded video elements back to the main thread
                 self.postMessage(preloadedVideos);
+            } else {
+                // Preload the next video
+                preloadIndex++;
+                if (preloadIndex < videoFileNames.length) {
+                    preloadVideo(videoFileNames[preloadIndex]);
+                }
             }
         });
     }
 
-    // Preload all videos in parallel
-    videoFileNames.forEach((videoFileName, index) => {
-        preloadVideo(videoFileName, index);
-    });
+    // Start preloading the first video
+    if (preloadIndex < videoFileNames.length) {
+        preloadVideo(videoFileNames[preloadIndex]);
+    }
 });
