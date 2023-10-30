75c1d5c585bbdc74adb71ae048d212cd8074679e
tomorrowTV <146773400+tomorrowTV@users.noreply.github.com>
2023-10-30 14:43:31 -0400

    Update preloadWorker.js

--- preloadWorker.js
+++ preloadWorker.js
@@ -1,30 +1,34 @@
 self.addEventListener('message', event => {
     const videoFileNames = event.data;
 
-    // Array to hold preloaded video elements
-    const preloadedVideos = [];
+    if (Array.isArray(videoFileNames)) {
+        // Array to hold preloaded video elements
+        const preloadedVideos = [];
 
-    // Function to preload a single video
-    function preloadVideo(videoFileName, index) {
-        const videoPath = 'wwwroot/videos/' + videoFileName;
-        const preloadVideo = document.createElement('video');
-        preloadVideo.src = videoPath;
-        preloadVideo.preload = 'auto';
+        // Function to preload a single video
+        function preloadVideo(videoFileName, index) {
+            const videoPath = 'wwwroot/videos/' + videoFileName;
+            const preloadVideo = document.createElement('video');
+            preloadVideo.src = videoPath;
+            preloadVideo.preload = 'auto';
 
-        preloadVideo.addEventListener('loadeddata', () => {
-            // Add the preloaded video element to the array
-            preloadedVideos[index] = preloadVideo;
+            preloadVideo.addEventListener('loadeddata', () => {
+                // Add the preloaded video element to the array
+                preloadedVideos[index] = preloadVideo;
 
-            // Check if all videos are preloaded
-            if (preloadedVideos.length === videoFileNames.length) {
-                // Post all preloaded video elements back to the main thread
-                self.postMessage(preloadedVideos);
-            }
+                // Check if all videos are preloaded
+                if (preloadedVideos.length === videoFileNames.length) {
+                    // Post all preloaded video elements back to the main thread
+                    self.postMessage(preloadedVideos);
+                }
+            });
+        }
+
+        // Preload all videos in parallel
+        videoFileNames.forEach((videoFileName, index) => {
+            preloadVideo(videoFileName, index);
         });
+    } else {
+        console.error('Invalid videoFileNames data:', videoFileNames);
     }
-
-    // Preload all videos in parallel
-    videoFileNames.forEach((videoFileName, index) => {
-        preloadVideo(videoFileName, index);
-    });
 });
